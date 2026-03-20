cc3094e77d1871df1035e2cf57cd25a915f0b3f4
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-03-20 19:27:46 -0300

    feat: Added support for color scheme in play/pause button

--- components/ui/PlayPauseButton.tsx
+++ components/ui/PlayPauseButton.tsx
@@ -1,6 +1,6 @@
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
 import { useState } from 'react'
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity, useColorScheme, View } from 'react-native'
 
 type Props = {
 	initialIsPlaying?: boolean
@@ -10,10 +10,12 @@ type Props = {
 
 export function PlayPauseButton({ initialIsPlaying, onPause, onResume }: Props) {
 	const [isPlaying, setIsPlaying] = useState(initialIsPlaying ?? false)
+	const colorScheme = useColorScheme()
+	const isDarkMode = colorScheme === 'dark'
 
 	return (
 		<TouchableOpacity
-			activeOpacity={0.7}
+			activeOpacity={0.5}
 			onPress={() =>
 				setIsPlaying((prev) => {
 					if (!prev && onResume) {
@@ -26,11 +28,21 @@ export function PlayPauseButton({ initialIsPlaying, onPause, onResume }: Props) 
 				})
 			}
 		>
-			<MaterialCommunityIcons
-				name={isPlaying ? 'pause' : 'play'}
-				size={24}
-				color="black"
-			/>
+			<View
+				style={{
+					backgroundColor: isDarkMode ? 'rgba(0, 0, 0, 0.4)' : 'rgba(255, 255, 255, 0.5)',
+					paddingHorizontal: 8,
+					paddingVertical: 8,
+					borderRadius: '50%',
+					mixBlendMode: 'overlay',
+				}}
+			>
+				<MaterialCommunityIcons
+					name={isPlaying ? 'pause' : 'play'}
+					size={24}
+					color={colorScheme === 'dark' ? 'white' : 'black'}
+				/>
+			</View>
 		</TouchableOpacity>
 	)
 }
