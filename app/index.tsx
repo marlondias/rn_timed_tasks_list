5cfb77317bbf17ad4c1b0b632d3aa509712f6ed2
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-03-22 17:24:49 -0300

    feat: Use tasks list prototype to main screen (mock data)

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,40 @@
-import { TaskItem } from "@/components/ui/TaskItem";
-import { Text, View } from "react-native";
+import { TaskList } from '@/components/TaskList'
+import { Task } from '@/types/Task'
+import { StyleSheet } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+
+function getRandomTimeInSeconds(): number {
+	const min = 30
+	const max = 2 * 60 * 60
+	return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+const mockTasks: Task[] = Array.from({ length: 20 }).map((value, index) => {
+	const completionTime = getRandomTimeInSeconds()
+	const remainingTime = Math.random() * completionTime
+	return {
+		id: 100 + index,
+		title: `Mock task #${index + 1}`,
+		createdAt: new Date(),
+		timeForCompletionInSeconds: completionTime,
+		remainingTimeInSeconds: remainingTime,
+		isRunning: false,
+	}
+})
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <TaskItem />
-    </View>
-  );
+	return (
+		<SafeAreaProvider>
+			<SafeAreaView style={styles.safeContainer}>
+				<TaskList tasks={mockTasks} />
+			</SafeAreaView>
+		</SafeAreaProvider>
+	)
 }
+
+const styles = StyleSheet.create({
+	safeContainer: {
+		flex: 1,
+		backgroundColor: '#C6E916',
+	},
+})
