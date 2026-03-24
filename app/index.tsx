04886c69b1b174d0a20e31d1d2619adafb0835d5
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-03-24 19:33:55 -0300

    refactor: Changed main screen to include add button and modal (mock)

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,69 @@
-import { TaskList } from '@/components/TaskList'
+import { CreateTaskModal } from '@/components/CreateTaskModal'
+import { AddTaskButton } from '@/components/ui/AddTaskButton'
 import { Task } from '@/types/Task'
-import { StyleSheet } from 'react-native'
+import { TimerDuration } from '@/types/TimerDuration'
+import { useState } from 'react'
+import { StyleSheet, useColorScheme, View } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
-function getRandomTimeInSeconds(): number {
-	const min = 30
-	const max = 2 * 60 * 60
-	return Math.floor(Math.random() * (max - min + 1)) + min
+function getRandomDuration(): TimerDuration {
+	return {
+		hours: Math.floor(Math.random() * 12),
+		minutes: Math.floor(Math.random() * 59),
+		seconds: Math.floor(Math.random() * 59),
+	}
 }
 
-const mockTasks: Task[] = Array.from({ length: 20 }).map((value, index) => {
-	const completionTime = getRandomTimeInSeconds()
-	const remainingTime = Math.random() * completionTime
+function getDurationAsSeconds(duration: TimerDuration): number {
+	const MINUTE_IN_SECONDS = 60
+	const HOUR_IN_SECONDS = 60 * MINUTE_IN_SECONDS
+	return (
+		duration.hours * HOUR_IN_SECONDS +
+		duration.minutes * MINUTE_IN_SECONDS +
+		duration.seconds
+	)
+}
+
+const mockTasks: Task[] = Array.from({ length: 4 }).map((value, index) => {
+	const duration = getRandomDuration()
+	const remainingTime = Math.floor(getDurationAsSeconds(duration) * Math.random())
 	return {
 		id: 100 + index,
 		title: `Mock task #${index + 1}`,
+		duration,
 		createdAt: new Date(),
-		timeForCompletionInSeconds: completionTime,
 		remainingTimeInSeconds: remainingTime,
 		isRunning: false,
 	}
 })
 
 export default function Index() {
+	const isDarkMode = useColorScheme() === 'dark'
+	const [isCreationModalVisible, setIsCreationModalVisible] = useState(false)
+
 	return (
 		<SafeAreaProvider>
 			<SafeAreaView style={styles.safeContainer}>
-				<TaskList tasks={mockTasks} />
+				<View
+					style={{
+						...styles.screenContainer,
+						...(isDarkMode
+							? styles.screenContainerDarkMode
+							: styles.screenContainerLightMode),
+					}}
+				>
+					{/* <TaskList tasks={mockTasks} /> */}
+
+					<CreateTaskModal
+						isVisible={isCreationModalVisible}
+						setIsVisible={setIsCreationModalVisible}
+						onConfirmCreation={(title, duration) => console.log({ title, duration })}
+					/>
+
+					<View style={styles.buttonOverlay}>
+						<AddTaskButton onPress={() => setIsCreationModalVisible(true)} />
+					</View>
+				</View>
 			</SafeAreaView>
 		</SafeAreaProvider>
 	)
@@ -36,5 +73,19 @@ const styles = StyleSheet.create({
 	safeContainer: {
 		flex: 1,
 		backgroundColor: '#C6E916',
+	},
+	screenContainer: {
+		flex: 1,
+	},
+	screenContainerLightMode: {
+		backgroundColor: '#eeeeee',
+	},
+	screenContainerDarkMode: {
+		backgroundColor: '#1e1e1e',
+	},
+	buttonOverlay: {
+		position: 'absolute',
+		right: 30,
+		bottom: 30,
 	},
 })
