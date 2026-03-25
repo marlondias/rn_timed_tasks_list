7179ac1482eb03de6ae2184d7ee0c95595d9d271
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-03-25 00:25:22 -0300

    fix: Correct theme management + SafeAreaProvider in the right place

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,21 @@
 import { TaskStorageProvider } from '@/contexts/TaskStorage/TaskStorageProvider'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { useColorScheme } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function RootLayout() {
+	const colorScheme = useColorScheme()
+
 	return (
 		<TaskStorageProvider>
-			<Stack />
+			<SafeAreaProvider>
+				<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+					<StatusBar style="auto" />
+					<Stack />
+				</ThemeProvider>
+			</SafeAreaProvider>
 		</TaskStorageProvider>
 	)
 }
