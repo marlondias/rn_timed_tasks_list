213b43a0dc6fbfcf64e09cb6a30102bfd3e5b16b
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-03-26 00:00:21 -0300

    feat: Included "edit" modal in the task list

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,12 +1,29 @@
 import { TaskItem } from '@/components/ui/TaskItem'
 import { useTaskStorage } from '@/contexts/TaskStorage/TaskStorageContext'
+import { Task } from '@/types/Task'
+import { useState } from 'react'
 import { ScrollView, StyleSheet, View } from 'react-native'
+import { EditTaskModal } from './EditTaskModal'
 
 export function TaskList() {
 	const { tasks, taskStorageService } = useTaskStorage()
+	const [isEditModalVisible, setIsEditModalVisible] = useState(false)
+	const [taskBeingEdited, setTaskBeingEdited] = useState<Task>()
 
 	return (
 		<View style={styles.container}>
+			{taskBeingEdited && (
+				<EditTaskModal
+					isVisible={isEditModalVisible}
+					setIsVisible={setIsEditModalVisible}
+					initialTitle={taskBeingEdited.title}
+					initialDuration={taskBeingEdited.duration}
+					onConfirmEditing={(title, duration) => {
+						taskStorageService.modify(taskBeingEdited.id, title, duration)
+					}}
+				/>
+			)}
+
 			<ScrollView contentContainerStyle={styles.scrollContent}>
 				{tasks.map((task, index) => (
 					<TaskItem
@@ -22,7 +39,9 @@ export function TaskList() {
 							console.log(`RESTART task ID=${taskId}`)
 						}}
 						onPressEdit={(taskId) => {
-							console.log(`Abrir modal de edição. Task ID=${taskId}`)
+							const task = taskStorageService.get(taskId)
+							setTaskBeingEdited(task)
+							setIsEditModalVisible(true)
 						}}
 						onPressDuplicate={(taskId) => {
 							taskStorageService.duplicate(taskId)
