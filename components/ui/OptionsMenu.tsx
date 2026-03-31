b4788eae874176a466550b486025b9243d6e2e9d
Marlon P. Dias <marlondias@users.noreply.github.com>
2026-03-31 00:26:58 -0300

    fix: Missing react dependency in component

--- components/ui/OptionsMenu.tsx
+++ components/ui/OptionsMenu.tsx
@@ -63,7 +63,7 @@ export function OptionsMenu({
 				onPressRemove()
 			}
 		},
-		[onPressEdit, onPressDuplicate, onPressRemove]
+		[allowEdit, onPressEdit, onPressDuplicate, onPressRemove]
 	)
 
 	return (
